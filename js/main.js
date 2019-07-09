ae25eb3bbd647f5b861c84dda7ce66a19e77bfe2
pinkGiraffe <30688225+pinkGiraffe@users.noreply.github.com>
2019-07-09 09:19:53 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#app',
   data: {
-    msg: 'Hello Vue!'
+    msg: 'Hello Vue!',
+    wheatPic:'http://img.mp.itc.cn/upload/20160405/eedeba847a6342dca763da669ae32413_th.jpg'
   }
 })
