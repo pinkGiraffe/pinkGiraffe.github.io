598d7c382821fee84ab0a8b30ce86c931a93a53f
pinkGiraffe <30688225+pinkGiraffe@users.noreply.github.com>
2019-07-15 08:57:05 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,19 @@ $(document).ready(function(){
         $("#login").css("display","none");
         $("#start").css("display","none");
     })
+    $("$introduction").click(function(){
+        $.ajax({
+            type:"get",
+            url:"../data/pai.txt",
+            dataType:"txt",
+            success:function(data){
+                if(data.success){
+                    $("#flower h1").html(data);
+                }
+                else{
+                    $("#flower h1").html("无法连接该书书目");
+                }
+            }
+        })
+    })
 })
